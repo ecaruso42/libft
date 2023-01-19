19fa69b058842c95d52536595670197a2f0c1e96
Eugenio Caruso <ecaruso@student.42roma.it>
2023-01-19 17:11:08 +0100

    hello

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,8 +6,24 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 16:03:55 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/01/19 16:05:23 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/01/19 17:09:11 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t len)
+{
+	unsigned char	*str;
+	unsigned char	*str1;
+	size_t			i;
+
+	i = 0;
+	str = (unsigned char *)dst;
+	str1 = (unsigned char *)src;
+	while (i < len)
+	{
+		str[1] = str1[1];
+		i++;
+	}
+}
