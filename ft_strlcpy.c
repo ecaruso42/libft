1720771eed59263e2950850400d5992ced4228d2
Eugenio Caruso <ecaruso@student.42roma.it>
2023-01-20 17:32:47 +0100

    hello

---
+++ ft_strlcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 16:59:46 by ecaruso           #+#    #+#             */
+/*   Updated: 2023/01/20 17:29:31 by ecaruso          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlen(char *str)
+{
+	int	a;
+
+	a = 0;
+	while (str[a] != '\0')
+		a++;
+	return (a);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	a;
+	unsigned int	b;
+
+	b = ft_strlen(src);
+	a = 0;
+	if (size != 0)
+	{
+		while (src[a] != '\0' && a < size - 1)
+		{
+			dest[a] = src[a];
+			a++;
+		}
+		dest[a] = '\0';
+	}
+	return (b);
+}
