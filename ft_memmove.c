6c7f9de945488001cd16b418ecf823b94012ad0f
Eugenio Caruso <ecaruso@student.42roma.it>
2023-01-21 18:13:48 +0100

    hello

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove(da finire).c                            :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 17:21:13 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/01/21 17:21:51 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/01/21 18:13:37 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	size_t	i;
 
+	i = 0;
+	if (!dest)
+		return (NULL);
+	if ((size_t)dest - (size_t)src < n)
+	{
+		i = n - 1;
+		while (i >= 0 && i < n)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i--;
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	return (dest);
 }
