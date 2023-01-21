30dad8efe0df87dcc46c1247c08224d6d5d511ea
Eugenio Caruso <ecaruso@student.42roma.it>
2023-01-21 18:45:33 +0100

    hello

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:16:48 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/01/21 17:24:01 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/01/21 18:45:21 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ char	*ft_strchr(const char *s, int c)
 	{
 		if (c == s[a])
 			return ((char *)(s + a));
+		i++;
 	}
+	if (c == '\0')
+		return ((char *)(str + 1));
 	return (NULL);
 }
