6fbb55f5965fe86f9f59a1fd1b04273a0dec951c
Eugenio Caruso <ecaruso@student.42roma.it>
2023-01-26 15:26:49 +0100

    hello

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,22 +6,26 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 15:03:38 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/01/26 15:07:24 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/01/26 15:25:30 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
 
+	ptr1 = (unsigned char *) s1;
+	ptr2 = (unsigned char *) s2;
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	while ((ptr1[i] != '\0' || ptr2[i] != '\0') && i < n)
 	{
-		if (s1[i] > s2[i])
+		if (ptr1[i] > ptr2[i])
 		{
 			return (1);
 		}
-		else if (s1[i] < s2[i])
+		else if (ptr1[i] < ptr2[i])
 		{
 			return (-1);
 		}
