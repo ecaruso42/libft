3c1b33955475bbb8970533a6a174f487dddf12ac
Eugenio Caruso <ecaruso@student.42roma.it>
2023-01-29 15:47:57 +0100

    hello

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 19:17:23 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/01/29 15:15:40 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/01/29 15:45:34 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ int				ft_strncmp(char *s1, char *s2, unsigned int n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 char			*ft_strnstr(const char *str, const char *to_find, size_t len);
+int				ft_atoi(const char *str);
 
 #endif
